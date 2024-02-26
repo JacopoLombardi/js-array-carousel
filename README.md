24ad8d7fc66b02775e91a2465279502368614bb5
JacopoLombardi <lombajacopo@gmail.com>
2024-02-26 17:16:52 +0100

    [add] mostro e nascondo le frecce

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ Carosello Mono Array
 ===
 1. Organizziamo l'HTML e CSS con uno schema statico.
 2. creiamo l'Array su Js e riportiamolo sull'HTML.
-3. Gestione dello slide delle immagini a seconda dei pulsanti premuti.
+3. Con la pressione del pulsante Up nascondiamo l'immagine corrente, incrementiamo il Counter e mostriamo l'immagine successiva.
+4. Con la pressione del pulsante Down nascondiamo l'immagine corrente, decrementiamo il Counter e mostriamo l'immagine precedente.
+5. nascondiamo arrowUp quando arriviamo all'ultima immagine.
+6. nascondiamo arrowDown quando il counter è uguale a 0.
--- js/script.js
+++ js/script.js
@@ -10,16 +10,72 @@ const images = [
 
 // variabili di Js collegate all'HTML
 const boxImages = document.querySelector('.box_images');
+const arrowUp = document.querySelector('.arrow_up');
+const arrowDown = document.querySelector('.arrow_down');
+
+// variabili Js
+let counter = 0;
 
 
+// creazione degli elementi HTML con classe 'd-none'
 for( let i = 0; i < images.length; i++){
-
    let counterImg = images[i];
    boxImages.innerHTML += `<img class="d-none" src="${counterImg}" alt="">`;
 }
 
+// creo un Array con dentro i tag 'img' dell'HTML 
+const HTMLCollection = document.getElementsByTagName('img');
+
+// rimuovo dall'Array la classe 'd-none' al primo elemento, perche deve essere visibile appena si carica la pagina
+HTMLCollection[0].classList.remove('d-none');
 
 
 
 
 
+
+// nascondo arrowDown di default
+arrowDown.classList.add('d-none');
+
+
+
+// al click del pulsante Up...
+arrowUp.addEventListener('click', function(){
+
+   // mostro arrowDown
+   arrowDown.classList.remove('d-none');
+
+
+   HTMLCollection[counter].classList.add('d-none');
+
+   counter++;
+
+   HTMLCollection[counter].classList.remove('d-none');
+
+
+   // se il contatore ha raggiunto l'ultima immagine, nascondi ArrowUp
+   if(counter === images.length - 1){
+      arrowUp.classList.add('d-none');
+   }
+
+})
+
+
+
+// al click del pulsante Down...
+arrowDown.addEventListener('click', function(){
+
+
+   HTMLCollection[counter].classList.add('d-none');
+
+   counter--;
+
+   HTMLCollection[counter].classList.remove('d-none');
+
+
+   // se il counter è uguale a 0 nascondi ArrowDown
+   if(counter === 0){
+      arrowDown.classList.add('d-none');
+   }
+
+})
